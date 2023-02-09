b841caed60f1f594bee791bc5aa7e50428d03f2e
Sherren Kalalo <90306316+SherrenKalalo@users.noreply.github.com>
2023-02-09 18:29:32 +0800

    Add files via upload
    Insert File

---
+++ members.js
@@ -0,0 +1,26 @@
+module.exports  = [
+    {   
+        id: 1, 
+        nim: 105022010006, 
+        nama: "Sherren", 
+        nomorTelp: "085335183819"
+    }, 
+    {   
+        id: 2, 
+        nim: 105022010006, 
+        nama: "Chattline", 
+        nomorTelp: "085335183819"
+    },
+    {   
+        id: 3, 
+        nim: 105022010006, 
+        nama: "Hendra", 
+        nomorTelp: "085335183819"
+    },
+    {   
+        id: 4, 
+        nim: 105022010006, 
+        nama: "Jerico", 
+        nomorTelp: "085335183819"
+    },
+]
